392b90672a40f09c228fb209acc67fe873684259
Xoisai <isaacrayment123@gmail.com>
2020-11-20 19:08:38 +0000

    added templates folder to contain html templates

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Isaac'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Test Flask Application</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
