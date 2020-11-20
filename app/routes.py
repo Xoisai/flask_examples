065522e22db869f042a84530d6127187e1329519
Xoisai <isaacrayment123@gmail.com>
2020-11-20 19:21:35 +0000

    added for loop to add d psots from users

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Isaac'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Test Flask Application</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    posts = [
+        {
+            'author': {'username': 'Some User'},
+            'body': 'This is a post of some description'
+        },
+        {
+            'author': {'username': 'Another User'},
+            'body': 'I have thoughts I think people care about!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
