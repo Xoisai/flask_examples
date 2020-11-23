cd9d28c3b36e29dcb69259a7bd0819dc2b42c615
Xoisai <isaacrayment123@gmail.com>
2020-11-23 14:42:44 +0000

    added login/logout functionality with flask_login package

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+# Read in configuration file
 app.config.from_object(Config)
+
+# Set up database objects
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# User login functionality
+login = LoginManager(app)
 
 from app import routes, models
